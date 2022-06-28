149e8dc3f2d563d2fb27bbec9ef536c33b497819
S0raWasTaken <s0ra@duck.com>
2022-06-28 04:47:17 -0500

    Add clear command

--- src/groups.rs
+++ src/groups.rs
@@ -1,6 +1,13 @@
-use crate::commands::general::{ping::PING_COMMAND, prefix::PREFIX_COMMAND};
+use crate::commands::{
+    general::{ping::PING_COMMAND, prefix::PREFIX_COMMAND},
+    util::clear::CLEAR_COMMAND,
+};
 use serenity::framework::standard::macros::group;
 
 #[group]
 #[commands(ping, prefix)]
 struct General;
+
+#[group]
+#[commands(clear)]
+struct Util;
